a66c03db9ac5d8f838175fa7d86eaf00403ecf4a
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-10-13 10:15:37 +0200

    feat(api): add leads listing

---
+++ pages/api/leads.js
@@ -0,0 +1,26 @@
+import handleError from '../../api/helpers/handleError'
+import ApiError from '../../api/libs/ApiError'
+import withAuthentication from '../../api/middlewares/withAuthentication'
+import withPrisma from '../../api/middlewares/withPrisma'
+
+const ERROR_PATH = 'pages/api/LeadsController()'
+
+async function LeadsController(req, res) {
+  if (req.method !== 'GET') {
+    handleError(new ApiError('Method not allowed.', 405, true), ERROR_PATH, res)
+
+    return
+  }
+
+  try {
+    const leads = await req.db.lead.findMany()
+
+    res.status(200).json({
+      data: leads,
+    })
+  } catch (err) {
+    handleError(err, ERROR_PATH, res)
+  }
+}
+
+export default withPrisma(withAuthentication(LeadsController))
