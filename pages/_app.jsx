79659ce9e8e2dd69cdf881be34d9699eb635e31c
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-10-13 10:17:42 +0200

    style(app): add custom CSS reset

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import { GlobalStyle, ThemeProvider } from '@ivangabriele/singularity'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { createGlobalStyle } from 'styled-components'
 
 import withApi from '../app/hocs/withApi'
 import withAuth from '../app/hocs/withAuth'
@@ -9,16 +10,31 @@ import '@fontsource/poppins/300.css'
 import '@fontsource/poppins/400.css'
 import '@fontsource/poppins/500.css'
 
+const GlobalStyleCustom = createGlobalStyle`
+  html {
+    display: flex;
+    height: 100%;
+  }
+
+  body,
+  #__next,
+  #__laa {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+`
+
 export default function LabAgoraApp({ Component, pageProps }) {
   const WrappedComponent = withAuth(withApi(Component))
   const { pathname } = useRouter()
 
   if (pathname.startsWith('/admin') && !process.browser) {
-    return <div suppressHydrationWarning />
+    return <div id="__laa" suppressHydrationWarning />
   }
 
   return (
-    <div suppressHydrationWarning>
+    <div id="__laa" suppressHydrationWarning>
       <Head>
         <title>Lab Agora</title>
 
@@ -27,6 +43,7 @@ export default function LabAgoraApp({ Component, pageProps }) {
 
       <ThemeProvider>
         <GlobalStyle />
+        <GlobalStyleCustom />
 
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <WrappedComponent {...pageProps} />
