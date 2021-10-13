7f7db261f84460b72bdbaf93355ae6c6664f8c5d
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-10-13 10:25:46 +0200

    style(app): normalize Board boxing & title

--- app/templates/Board.jsx
+++ app/templates/Board.jsx
@@ -1,11 +1,47 @@
 import { Tasker } from '@ivangabriele/singularity'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+import AtomTitle from '../atoms/Title'
+import useApi from '../hooks/useApi'
+import useIsMounted from '../hooks/useIsMounted'
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 0;
+`
+
+const Title = styled(AtomTitle)`
+  padding-left: 1rem;
+`
 
 const Project = () => <Tasker.Task>123</Tasker.Task>
 
 export default function Board() {
+  const api = useApi()
+  const [isSynchronizing, setIsSynchronizing] = useState(false)
+  const isMounted = useIsMounted()
+
+  const synchronizePipedrive = async () => {
+    setIsSynchronizing(true)
+
+    await api.get('pipedrive/synchronize')
+
+    if (isMounted()) {
+      setIsSynchronizing(false)
+    }
+  }
+
+  useEffect(() => {
+    synchronizePipedrive()
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <>
-      <h2>Board</h2>
+    <Box>
+      <Title>Mise en relation {isSynchronizing ? `(Synchronizing...)` : null}</Title>
 
       <Tasker
         data={[
@@ -14,6 +50,6 @@ export default function Board() {
           { label: 'Completed', tasks: [Project] },
         ]}
       />
-    </>
+    </Box>
   )
 }
