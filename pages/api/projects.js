385a9558baf8e84d349a6bbde3154deb99b460bf
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-10-13 10:22:51 +0200

    feat(api): add projects listing

---
+++ pages/api/projects.js
@@ -0,0 +1,26 @@
+import handleError from '../../api/helpers/handleError'
+import ApiError from '../../api/libs/ApiError'
+import withAuthentication from '../../api/middlewares/withAuthentication'
+import withPrisma from '../../api/middlewares/withPrisma'
+
+const ERROR_PATH = 'pages/api/OrganizationsController()'
+
+async function OrganizationsController(req, res) {
+  if (req.method !== 'GET') {
+    handleError(new ApiError('Method not allowed.', 405, true), ERROR_PATH, res)
+
+    return
+  }
+
+  try {
+    const projects = await req.db.project.findMany()
+
+    res.status(200).json({
+      data: projects,
+    })
+  } catch (err) {
+    handleError(err, ERROR_PATH, res)
+  }
+}
+
+export default withPrisma(withAuthentication(OrganizationsController))
