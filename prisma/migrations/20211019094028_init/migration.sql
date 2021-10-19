9881e7b8d2cc7042f3b0c51034680f6a36806f83
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-10-19 11:41:05 +0200

    feat(fb): add note field to persons & orgs

---
+++ prisma/migrations/20211019094028_init/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Contributor" ADD COLUMN     "note" TEXT;
+
+-- AlterTable
+ALTER TABLE "Lead" ADD COLUMN     "note" TEXT;
+
+-- AlterTable
+ALTER TABLE "Organization" ADD COLUMN     "note" TEXT;
