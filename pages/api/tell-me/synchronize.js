79766ac5a71b332a98f452977d3b61466738e2dd
Florian Briand <florian.briand@digital-engine.info>
2022-06-16 11:40:51 +0200

    feat(app): map submissions values to contributor entry through question keys

--- pages/api/tell-me/synchronize.js
+++ pages/api/tell-me/synchronize.js
@@ -7,12 +7,78 @@ import withAuthentication from '../../../api/middlewares/withAuthentication'
 import withPrisma from '../../../api/middlewares/withPrisma'
 import { USER_ROLE } from '../../../common/constants'
 
-const ERROR_PATH = 'pages/api/tell-me/synchronize.js'
-/** @type {"NONE"|"RAW"|"CONSOLIDATED"} */
-const SUBMISSION_PARSING_MODE = 'RAW'
-const SYNCHRO_START_DATE = Temporal.Instant.from('2022-06-10T00:00:00.000Z')
+/**
+ * @typedef FormattedAnswer
+ * @property {string} id - question id
+ * @property {string} key - question field key
+ * @property {string} label - question field label
+ * @property {string} value - answer value
+ */
 
-function formatSubmissionForNotes(submission) {
+/**
+ * @typedef FormattedSubmission
+ * @property {Array<FormattedAnswer>} answers - formatted answers
+ * @property {string} id - submission id
+ * @property {string} openedAt - submission opening ISO date
+ * @property {string} submittedAt - submission ISO date
+ */
+
+const ERROR_PATH = 'pages/api/tell-me/synchronize.js'
+
+const SYNCHRO_START_DATE = Temporal.Instant.from('2022-06-16T00:00:00.000Z')
+
+/** @type {"NONE"|"RAW"|"CONSOLIDATED"} */
+const SUBMISSION_PARSING_MODE = 'CONSOLIDATED'
+const CONTRIBUTORS_FIELD_MAP = {
+  // TODO: add CONTRIBUTOR_TYPE handling
+  email: 'EMAIL',
+  firstName: 'FIRSTNAME',
+  lastName: 'LASTNAME',
+  phone: 'PHONE',
+}
+
+/**
+ * @param {string} fieldName
+ * @param {FormattedSubmission} submission
+ * @returns {string}
+ */
+function getMappedFieldValue(fieldName, submission) {
+  const mappedAnswer = submission.answers.find(answer => {
+    const mappedKey = CONTRIBUTORS_FIELD_MAP[fieldName]
+
+    return mappedKey === answer.key
+  })
+
+  return mappedAnswer?.value ?? '...'
+}
+
+/**
+ * @param {string} fieldName
+ * @param {FormattedSubmission} submission
+ * @returns {string}
+ */
+function getFieldValue(fieldName, submission) {
+  switch (SUBMISSION_PARSING_MODE) {
+    case 'CONSOLIDATED':
+      return getMappedFieldValue(fieldName, submission)
+    case 'RAW':
+    case 'NONE':
+    default:
+      switch (fieldName) {
+        case 'firstName':
+          return submission.id
+        default:
+          return '...'
+      }
+  }
+}
+
+/**
+ * @param {*} contributor
+ * @param {FormattedSubmission} submission
+ * @returns {string}
+ */
+function formatSubmissionForNotes(contributor, submission) {
   let notes
   switch (SUBMISSION_PARSING_MODE) {
     case 'RAW':
@@ -29,6 +95,24 @@ function formatSubmissionForNotes(submission) {
   return JSON.stringify(notes, null, 2)
 }
 
+/**
+ * @param {FormattedSubmission} submission
+ */
+function extractSubmissionForContributor(submission) {
+  const contributor = {
+    email: getFieldValue('email', submission),
+    firstName: getFieldValue('firstName', submission),
+    lastName: getFieldValue('lastName', submission),
+    phone: getFieldValue('phone', submission),
+  }
+
+  return {
+    ...contributor,
+    note: formatSubmissionForNotes(contributor, submission),
+    synchronizationId: submission.id,
+  }
+}
+
 async function createSynchronization(req, success, info) {
   const userId = req.me.id
 
@@ -41,15 +125,14 @@ async function createSynchronization(req, success, info) {
   })
 }
 
+/**
+ * @param {FormattedSubmission} submission
+ * @param {*} req
+ * @returns
+ */
 async function createContributor(submission, req) {
   return req.db.contributor.create({
-    data: {
-      email: '...',
-      firstName: submission.id,
-      lastName: '...',
-      note: formatSubmissionForNotes(submission),
-      synchronizationId: submission.id,
-    },
+    data: extractSubmissionForContributor(submission),
   })
 }
 
@@ -71,14 +154,27 @@ async function checkContributorNotSynchronized(submission, req) {
   }
 }
 
+/**
+ * @param {*} answers
+ * @returns {Array<FormattedAnswer>}
+ */
 function extractAnswers(answers) {
-  return answers.map(answer => ({
-    id: answer.question.id,
-    label: answer.question.value,
-    value: answer.rawValue,
-  }))
+  return answers.map(
+    /** @returns {FormattedAnswer} */
+    answer => ({
+      id: answer.question.id,
+      key: answer.question.key,
+      label: answer.question.value,
+      value: answer.rawValue,
+    }),
+  )
 }
 
+/**
+ *
+ * @param {*} submission
+ * @returns {FormattedSubmission}
+ */
 function formatSubmission(submission) {
   return {
     answers: extractAnswers(submission.answers),
